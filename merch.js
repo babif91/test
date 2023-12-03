8ba60353d9e34b48340152c361901e141881643c
babif91 <143929543+babif91@users.noreply.github.com>
2023-12-04 02:20:37 +0900

    Add files via upload

---
+++ merch.js
@@ -0,0 +1,15 @@
+const container = document.getElementById("container");
+const img = document.querySelector("img");
+container.addEventListener("mousemove", onZoom);
+container.addEventListener("mouseover", onZoom);
+container.addEventListener("mouseleave", offZoom);
+function onZoom(e) {
+    const x = e.clientX - e.target.offsetLeft;
+    const y = e.clientY - e.target.offsetTop;
+    img.style.transformOrigin = `${x}px ${y}px`;
+    img.style.transform = "scale(2.8)";
+}
+function offZoom(e) {
+    img.style.transformOrigin = `center center`;
+    img.style.transform = "scale(1)";
+}
