d3fabeac85468ce38bb79cc2686352bb3e1542db
Babi <babifukuoka@gmail.com>
2023-12-10 23:43:49 +0900

    new merch folder added

---
+++ merch/merch.js
@@ -0,0 +1,36 @@
+const container1 = document.getElementById("container1");
+const container2 = document.getElementById("container2");
+
+const img1 = container1.querySelector("img");
+const img2 = container2.querySelector("img");
+
+container1.addEventListener("mousemove", onZoom1);
+container1.addEventListener("mouseover", onZoom1);
+container1.addEventListener("mouseleave", offZoom1);
+
+container2.addEventListener("mousemove", onZoom2);
+container2.addEventListener("mouseover", onZoom2);
+container2.addEventListener("mouseleave", offZoom2);
+//img 1 function//
+function onZoom1(e) {
+    const x = e.clientX - e.target.offsetLeft;
+    const y = e.clientY - e.target.offsetTop;
+    img1.style.transformOrigin = `${x}px ${y}px`;
+    img1.style.transform = "scale(2.8)";
+}
+function offZoom1(e) {
+    img1.style.transformOrigin = `center center`;
+    img1.style.transform = "scale(1)";
+}
+
+//img 2 function//
+function onZoom2(e) {
+    const x = e.clientX - e.target.offsetLeft;
+    const y = e.clientY - e.target.offsetTop;
+    img2.style.transformOrigin = `${x}px ${y}px`;
+    img2.style.transform = "scale(2.8)";
+}
+function offZoom2(e) {
+    img2.style.transformOrigin = `center center`;
+    img2.style.transform = "scale(1)";
+}
