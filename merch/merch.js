e5edc256fdd87b8b1e909b1237f019ca1887e365
Babi <babifukuoka@gmail.com>
2023-12-13 02:05:57 +0900

    add beanies image

--- merch/merch.js
+++ merch/merch.js
@@ -1,8 +1,10 @@
 const container1 = document.getElementById("container1");
 const container2 = document.getElementById("container2");
+const container3 = document.getElementById("container2");
 
 const img1 = container1.querySelector("img");
 const img2 = container2.querySelector("img");
+const img3 = container3.querySelector("img");
 
 container1.addEventListener("mousemove", onZoom1);
 container1.addEventListener("mouseover", onZoom1);
@@ -11,6 +13,11 @@ container1.addEventListener("mouseleave", offZoom1);
 container2.addEventListener("mousemove", onZoom2);
 container2.addEventListener("mouseover", onZoom2);
 container2.addEventListener("mouseleave", offZoom2);
+
+container3.addEventListener("mousemove", onZoom3);
+container3.addEventListener("moveover", onZoom3);
+container3.addEventListener("mouseleave", offZoom3);
+
 //img 1 function//
 function onZoom1(e) {
     const x = e.clientX - e.target.offsetLeft;
@@ -33,4 +40,16 @@ function onZoom2(e) {
 function offZoom2(e) {
     img2.style.transformOrigin = `center center`;
     img2.style.transform = "scale(1)";
+}
+
+//img 3 function//
+function onZoom3(e) {
+    const x = e.clientX - e.target.offsetLeft;
+    const y = e.clientY - e.target.offsetTop;
+    img3.style.transformOrigin = `${x}px ${y}px`;
+    img3.style.transform = "scale(2.8)";
+}
+function offZoom2(e) {
+    img3.style.transformOrigin = `center center`;
+    img3.style.transform = "scale(1)";
 }
